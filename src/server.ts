e5b6e792e4a64001070241f44d3ad81095b98849
Arnav Gupta <arnav.gupta@scaler.com>
2021-11-22 13:32:04 +0530

    fix: fix Eslint settings
    Signed-off-by: Arnav Gupta <arnav.gupta@scaler.com>

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import express from "express"
-import userRoute from "./routes/users"
-import urlRoute from "./routes/urls"
+import express from 'express'
+import userRoute from './routes/users'
+import urlRoute from './routes/urls'
 
 const app = express()
 
-app.use("/urls", urlRoute)
-app.use("/users", userRoute)
+app.use('/urls', urlRoute)
+app.use('/users', userRoute)
 
-app.get("/", (req, res) => {
-    res.send("Hello TS Server")
+app.get('/', (req, res) => {
+  res.send('Hello TS Server')
 })
 
 app.listen(5678, () => {
-    console.log("Server is running on http://localhost:5678")
+  console.log('Server is running on http://localhost:5678')
 })
